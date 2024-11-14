d6dc32d6c11380237ada898990cd1be184fd3af3
cescgreoles <cesc.greoles@gmail.com>
2024-11-14 12:14:21 +0100

    maria-ts push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 
 export default function Home() {
   const years = [2016, 2020, 2021, 2022, 2023, 2024];
-
   const backgroundImages = ["/image/2016/1/1.webp", "/image/2016/1/2.webp"];
 
   const [currentBackground, setCurrentBackground] = useState(0);
@@ -28,11 +27,15 @@ export default function Home() {
         }}
       ></div>
 
-      <div className="flex flex-col justify-center items-center z-10 text-white mt-32 px-6 flex-grow">
-        <p className="text-lg sm:text-xl font-light text-center max-w-3xl">
-          Este es un espacio donde podrás explorar los proyectos más destacados
-          de María Torrecillas. Un lugar para la innovación, creatividad y
-          soluciones únicas.
+      <div className="flex flex-col z-10 text-white mt-32 px-6 flex-grow">
+        <p className="text-lg sm:text-xl font-light text-center max-w-3xl italic">
+          &quot;Elijo elementos de la naturaleza que por cualquier motivo atraen
+          mi interés, escogiendo lo que a menudo nos pasa desapercibido. Para
+          sorprender al espectador, hay que transportarlo a un universo
+          imaginario, a otro espacio donde redescubra su mirada. Que pueda ver
+          de otra manera elementos que siempre han estado allí, que descubra la
+          luz y la sombra sobre pequeños detalles que pasan desapercibidos y se
+          conviertan en elementos esenciales&quot;
         </p>
       </div>
 
@@ -45,38 +48,46 @@ export default function Home() {
             height={40}
             style={{ width: "auto", height: "auto" }}
           />
-          <h1 className="text-xl sm:text-3xl font-extrabold text-white">
-            MARIA TORRECILLAS
-          </h1>
+          <div className="flex flex-col">
+            <h1 className="text-xl sm:text-3xl font-extrabold text-white">
+              MARIA TORRECILLAS
+            </h1>
+            <p className="text-white">FOTOGRAFÍA</p>
+          </div>
         </div>
 
         <Link
           href="/contacto"
-          className="text-white uppercase text-lg hover:underline"
+          className="text-white uppercase text-lg border border-white py-1 px-2 transition duration-300 ease-in-out hover:bg-white hover:bg-opacity-20 hover:text-gray-100"
         >
-          CONTACTO
+          Contacto
         </Link>
       </div>
 
-      <div className="flex justify-center px-6 mt-32 pb-10 flex-grow">
-        <div className="flex flex-wrap justify-center gap-6 w-full max-w-screen-xl">
+      <div className="flex justify-center mt-32 p-4">
+        <div className="flex flex-wrap justify-center gap-3 w-full max-w-screen-xl">
           {years.map((year) => (
             <Link key={year} href={`/${year}`}>
-              <div className="relative group cursor-pointer rounded-lg overflow-hidden bg-gray-800 shadow-lg transform transition-all hover:scale-105 hover:shadow-xl w-28 sm:w-36 md:w-40">
-                <div className="relative w-full h-32 sm:h-36 md:h-40">
+              <div className="relative group cursor-pointer overflow-hidden bg-gray-800 shadow-lg transform transition-all hover:scale-105 hover:shadow-xl w-24 sm:w-28 md:w-32 lg:w-36">
+                <div className="relative w-full h-24 sm:h-28 md:h-32 lg:h-36">
                   <Image
                     src={`/image/${year}/1/1.webp`}
                     alt={`Proyectos de ${year}`}
                     fill
-                    sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+                    sizes="(max-width: 150px) 60vw, (max-width: 150px) 30vw, 33vw"
                     style={{ objectFit: "cover" }}
                     className="transition-opacity group-hover:opacity-90"
                   />
                 </div>
                 <div className="absolute inset-0 bg-black bg-opacity-20 transition-opacity group-hover:bg-opacity-40"></div>
-                <div className="absolute bottom-0 left-0 right-0 p-2 text-center">
-                  <p className="text-lg font-semibold text-white">{year}</p>
+
+                <div className="absolute bottom-0 left-0 right-0 p-2 text-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <p className="text-lg text-white">{year}</p>
                 </div>
+
+                <div
+                  className={`absolute inset-0 border-2 border-transparent group-hover:border-white transition-all duration-300`}
+                ></div>
               </div>
             </Link>
           ))}
