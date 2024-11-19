08e194fc910aa295c736819dc80ea286584a4997
cescgreoles <cesc.greoles@gmail.com>
2024-11-19 11:02:53 +0100

    preload push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,19 +14,26 @@ export default function Home() {
   ];
 
   const [currentBackground, setCurrentBackground] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentBackground((prev) => (prev + 1) % backgroundImages.length);
+      setIsTransitioning(true);
+      setTimeout(() => {
+        setCurrentBackground((prev) => (prev + 1) % backgroundImages.length);
+        setIsTransitioning(false);
+      }, 1000);
     }, 5000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [backgroundImages.length]);
 
   return (
     <main className="min-h-screen flex flex-col relative">
       <div
-        className="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center transition-all"
+        className={`absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center transition-opacity duration-1000 ${
+          isTransitioning ? "opacity-0" : "opacity-100"
+        }`}
         style={{
           backgroundImage: `url(${backgroundImages[currentBackground]})`,
         }}
@@ -35,13 +42,12 @@ export default function Home() {
       <div className="flex items-center justify-between px-6 absolute top-6 left-0 right-0 z-10 w-full">
         <div className="flex items-center space-x-4">
           <div className="flex flex-col">
-            <h1 className="text-xl sm:text-3xl  text-white">
+            <h1 className="text-xl sm:text-3xl text-white">
               MARIA TORRECILLAS
             </h1>
             <p className="text-white">FOTOGRAFÍA</p>
           </div>
         </div>
-
         <Link
           href="/contacto"
           className="text-white uppercase text-lg border border-white py-1 px-2 transition duration-300 ease-in-out hover:bg-white hover:bg-opacity-20 hover:text-gray-100"
